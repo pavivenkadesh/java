e9cb45154a23299d4893267a354c323d0119efd5
pavivenkadesh <43901922+pavivenkadesh@users.noreply.github.com>
2019-02-11 14:11:58 +0530

    Add files via upload

--- matrix.java
+++ matrix.java
@@ -1,47 +1,34 @@
-import java.util.Scanner;
-
- class matrix {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int m=sc.nextInt();
-		int l=0,p=0;
-		int a[][]=new int[n][m];
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<m;j++)
-			{
-				a[i][j]=sc.nextInt();
-			}
-		}
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<m;j++)
-			{
-				if(a[i][j]==0)
-				{
-					for(l=0,p=0;l<m&&p<n;l++,p++)
-					{
-						a[i][l]='*';
-						a[p][j]='*';
-					}
-				}
-			}
-		}
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<m;j++)
-			{
-				if(a[i][j]=='*')
-				{
-					a[i][j]=0;
-				}
-				System.out.print(a[i][j]+" ");
-			}
-			System.out.println();
-		}
-		sc.close();
-	}
-
+import java.util.*;
+import java.math.*;
+class matrix{
+    public static void main(String aa[]){
+        int l,k;
+       Scanner s=new Scanner(System.in);
+       int n=s.nextInt();
+       int m=s.nextInt();
+       int a[][]=new int[n][m];
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               a[i][j]=s.nextInt();
+           }
+       }
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               if(a[i][j]==0){
+                   for( k=0;k<m;k++){
+                       a[i][k]=0;
+                   }
+                   for(l=0;l<n;l++){
+                       a[l][j]=0;
+                   }
+               }
+           }
+       }
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               System.out.println(a[i][j]);
+           }
+           
+       }
+    }
 }
